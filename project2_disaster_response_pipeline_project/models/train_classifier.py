b61cf9d0104c5f3551f56baecae93e539eabc0f6
Sophie Luethke <q356550@lptp2002619.speedport.ip>
2024-01-20 19:53:18 +0100

    updates in parameters

--- project2_disaster_response_pipeline_project/models/train_classifier.py
+++ project2_disaster_response_pipeline_project/models/train_classifier.py
@@ -125,9 +125,8 @@ def build_model():
                         ])
 
     parameters = {
-                    'tfidf__use_idf': [True, False],
                     'clf__estimator__n_estimators': [40, 50],
-                    'clf__estimator__min_samples_leaf':[2, 5],
+                    'clf__estimator__min_samples_leaf':[2, 5]
                     }
 
     # create grid search object
